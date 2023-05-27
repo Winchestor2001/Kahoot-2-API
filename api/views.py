2e3a6f9a60b8a5dbee3b013e6a811145c6ed9d6d
winchestor2001 <bexruz2001xx@gmail.com>
2023-05-27 13:02:02 +0500

    update project

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from rest_framework import generics
 
-# Create your views here.
+
+class HomeView(APIView):
+    def get(self, request):
+        return Response({}, status=200)
